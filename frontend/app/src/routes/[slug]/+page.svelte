8f17b9c6346cb2922850e399078df3b261c42fee
nishitjainalt <nijain@tcd.ie>
2024-02-24 15:23:27 +0000

    fixed icons

--- frontend/app/src/routes/[slug]/+page.svelte
+++ frontend/app/src/routes/[slug]/+page.svelte
@@ -11,44 +11,15 @@
 			<a href="/" class="btn btn-ghost text-xl">Group10</a>
 		</div>
 		<div class="flex-none">
-			<div class="dropdown dropdown-end">
-				<div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-					<div class="indicator">
-						<i class="fa-solid fa-chart-simple"></i>
-					</div>
-				</div>
-				<div
-					tabindex="0"
-					class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-				>
-					<div class="card-body">
-						<span class="font-bold text-lg">8 Items</span>
-						<span class="text-info">Subtotal: $999</span>
-						<div class="card-actions">
-							<button class="btn btn-primary btn-block">View cart</button>
-						</div>
-					</div>
+			<div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+				<div class="indicator">
+					<i class="fa-solid fa-chart-simple"></i>
 				</div>
 			</div>
-			<div class="dropdown dropdown-end">
-				<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-					<div>
-						<i class="fa-solid fa-music"></i>
-					</div>
+			<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+				<div>
+					<i class="fa-solid fa-music"></i>
 				</div>
-				<ul
-					tabindex="0"
-					class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-				>
-					<li>
-						<a class="justify-between">
-							Profile
-							<span class="badge">New</span>
-						</a>
-					</li>
-					<li><a>Settings</a></li>
-					<li><a>Logout</a></li>
-				</ul>
 			</div>
 		</div>
 	</div>
