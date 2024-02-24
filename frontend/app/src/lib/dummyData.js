38b1fcf2a59957ae8ad7fe915cca32be49e6e549
Hari <hprasad2709@gmail.com>
2024-02-24 11:00:32 +0000

    barebones list page created

---
+++ frontend/app/src/lib/dummyData.js
@@ -0,0 +1,52 @@
+export default [
+	{
+		title: 'vulputate',
+		artist: 'Conni McWard',
+		url: 'http://eepurl.com/nibh/in/quis/justo.json?lorem=platea&integer=dictumst&tincidunt=maecenas&ante=ut&vel=massa&ipsum=quis&praesent=augue&blandit=luctus&lacinia=tincidunt&erat=nulla&vestibulum=mollis&sed=molestie&magna=lorem&at=quisque&nunc=ut&commodo=erat&placerat=curabitur&praesent=gravida&blandit=nisi&nam=at&nulla=nibh&integer=in&pede=hac&justo=habitasse&lacinia=platea&eget=dictumst&tincidunt=aliquam&eget=augue&tempus=quam&vel=sollicitudin&pede=vitae&morbi=consectetuer&porttitor=eget&lorem=rutrum&id=at&ligula=lorem&suspendisse=integer&ornare=tincidunt&consequat=ante&lectus=vel&in=ipsum&est=praesent&risus=blandit&auctor=lacinia&sed=erat&tristique=vestibulum&in=sed&tempus=magna&sit=at&amet=nunc&sem=commodo&fusce=placerat&consequat=praesent&nulla=blandit&nisl=nam&nunc=nulla&nisl=integer&duis=pede&bibendum=justo&felis=lacinia&sed=eget&interdum=tincidunt&venenatis=eget&turpis=tempus&enim=vel&blandit=pede&mi=morbi&in=porttitor&porttitor=lorem&pede=id&justo=ligula&eu=suspendisse&massa=ornare&donec=consequat&dapibus=lectus&duis=in&at=est&velit=risus&eu=auctor&est=sed&congue=tristique&elementum=in&in=tempus&hac=sit&habitasse=amet&platea=sem&dictumst=fusce'
+	},
+	{
+		title: 'mattis pulvinar',
+		artist: 'Malia Frobisher',
+		url: 'https://uol.com.br/sociis.jpg?justo=donec&lacinia=ut&eget=mauris&tincidunt=eget&eget=massa&tempus=tempor&vel=convallis&pede=nulla&morbi=neque&porttitor=libero&lorem=convallis&id=eget&ligula=eleifend&suspendisse=luctus&ornare=ultricies&consequat=eu&lectus=nibh&in=quisque&est=id&risus=justo&auctor=sit&sed=amet&tristique=sapien&in=dignissim&tempus=vestibulum&sit=vestibulum&amet=ante&sem=ipsum&fusce=primis&consequat=in&nulla=faucibus&nisl=orci&nunc=luctus&nisl=et&duis=ultrices&bibendum=posuere&felis=cubilia&sed=curae&interdum=nulla&venenatis=dapibus&turpis=dolor&enim=vel&blandit=est&mi=donec&in=odio&porttitor=justo&pede=sollicitudin&justo=ut&eu=suscipit&massa=a&donec=feugiat&dapibus=et&duis=eros&at=vestibulum&velit=ac'
+	},
+	{
+		title: 'justo aliquam',
+		artist: 'Jolynn Pieroni',
+		url: 'http://com.com/ut/mauris.aspx?montes=erat&nascetur=nulla&ridiculus=tempus&mus=vivamus&vivamus=in&vestibulum=felis&sagittis=eu&sapien=sapien&cum=cursus&sociis=vestibulum&natoque=proin&penatibus=eu&et=mi&magnis=nulla&dis=ac&parturient=enim&montes=in&nascetur=tempor&ridiculus=turpis&mus=nec&etiam=euismod&vel=scelerisque&augue=quam&vestibulum=turpis&rutrum=adipiscing&rutrum=lorem&neque=vitae&aenean=mattis&auctor=nibh&gravida=ligula&sem=nec&praesent=sem'
+	},
+	{
+		title: 'tincidunt lacus at',
+		artist: 'Auroora Matuska',
+		url: 'https://virginia.edu/ridiculus/mus.aspx?augue=faucibus&luctus=cursus&tincidunt=urna&nulla=ut&mollis=tellus&molestie=nulla&lorem=ut&quisque=erat&ut=id&erat=mauris&curabitur=vulputate&gravida=elementum&nisi=nullam&at=varius&nibh=nulla&in=facilisi&hac=cras&habitasse=non&platea=velit&dictumst=nec&aliquam=nisi&augue=vulputate&quam=nonummy&sollicitudin=maecenas&vitae=tincidunt&consectetuer=lacus&eget=at&rutrum=velit&at=vivamus&lorem=vel&integer=nulla&tincidunt=eget&ante=eros&vel=elementum&ipsum=pellentesque&praesent=quisque&blandit=porta&lacinia=volutpat&erat=erat&vestibulum=quisque&sed=erat&magna=eros&at=viverra&nunc=eget&commodo=congue&placerat=eget&praesent=semper&blandit=rutrum&nam=nulla&nulla=nunc&integer=purus&pede=phasellus&justo=in&lacinia=felis&eget=donec&tincidunt=semper&eget=sapien&tempus=a&vel=libero&pede=nam&morbi=dui&porttitor=proin&lorem=leo&id=odio&ligula=porttitor&suspendisse=id&ornare=consequat&consequat=in&lectus=consequat&in=ut&est=nulla&risus=sed&auctor=accumsan&sed=felis&tristique=ut&in=at&tempus=dolor&sit=quis&amet=odio&sem=consequat&fusce=varius&consequat=integer&nulla=ac&nisl=leo&nunc=pellentesque&nisl=ultrices'
+	},
+	{
+		title: 'nunc proin at',
+		artist: 'Anatol Klouz',
+		url: 'https://addthis.com/nisi/volutpat/eleifend/donec.jpg?turpis=leo&adipiscing=pellentesque&lorem=ultrices&vitae=mattis&mattis=odio&nibh=donec&ligula=vitae&nec=nisi&sem=nam&duis=ultrices&aliquam=libero&convallis=non&nunc=mattis&proin=pulvinar&at=nulla&turpis=pede&a=ullamcorper&pede=augue&posuere=a&nonummy=suscipit&integer=nulla&non=elit&velit=ac&donec=nulla&diam=sed&neque=vel&vestibulum=enim&eget=sit&vulputate=amet'
+	},
+	{
+		title: 'vel pede',
+		artist: 'Genna Furzey',
+		url: 'http://51.la/massa/id/nisl/venenatis.aspx?risus=in&dapibus=sagittis&augue=dui&vel=vel&accumsan=nisl&tellus=duis&nisi=ac&eu=nibh&orci=fusce&mauris=lacus&lacinia=purus&sapien=aliquet&quis=at&libero=feugiat&nullam=non&sit=pretium&amet=quis&turpis=lectus&elementum=suspendisse&ligula=potenti&vehicula=in&consequat=eleifend&morbi=quam&a=a&ipsum=odio&integer=in&a=hac&nibh=habitasse&in=platea&quis=dictumst&justo=maecenas&maecenas=ut&rhoncus=massa&aliquam=quis&lacus=augue&morbi=luctus&quis=tincidunt&tortor=nulla&id=mollis&nulla=molestie&ultrices=lorem&aliquet=quisque&maecenas=ut&leo=erat&odio=curabitur&condimentum=gravida&id=nisi&luctus=at&nec=nibh&molestie=in&sed=hac&justo=habitasse&pellentesque=platea&viverra=dictumst&pede=aliquam&ac=augue&diam=quam&cras=sollicitudin&pellentesque=vitae&volutpat=consectetuer&dui=eget&maecenas=rutrum&tristique=at&est=lorem&et=integer&tempus=tincidunt&semper=ante&est=vel&quam=ipsum'
+	},
+	{
+		title: 'in hac habitasse',
+		artist: 'Blisse Carr',
+		url: 'https://wikipedia.org/tellus/nulla/ut/erat.js?dis=nulla'
+	},
+	{
+		title: 'suscipit nulla',
+		artist: 'Yul Isles',
+		url: 'http://github.io/donec/pharetra.jsp?tempus=nibh&vel=quisque&pede=id&morbi=justo&porttitor=sit&lorem=amet&id=sapien&ligula=dignissim&suspendisse=vestibulum&ornare=vestibulum&consequat=ante&lectus=ipsum&in=primis&est=in&risus=faucibus&auctor=orci&sed=luctus&tristique=et&in=ultrices&tempus=posuere&sit=cubilia&amet=curae&sem=nulla&fusce=dapibus&consequat=dolor&nulla=vel&nisl=est&nunc=donec&nisl=odio&duis=justo&bibendum=sollicitudin&felis=ut&sed=suscipit&interdum=a'
+	},
+	{
+		title: 'justo sollicitudin',
+		artist: 'Wally Peagrim',
+		url: 'https://odnoklassniki.ru/eu/interdum/eu/tincidunt/in/leo.json?mi=sapien&nulla=a&ac=libero&enim=nam&in=dui&tempor=proin&turpis=leo&nec=odio&euismod=porttitor&scelerisque=id&quam=consequat&turpis=in&adipiscing=consequat&lorem=ut&vitae=nulla&mattis=sed&nibh=accumsan&ligula=felis&nec=ut&sem=at&duis=dolor&aliquam=quis&convallis=odio&nunc=consequat&proin=varius&at=integer&turpis=ac&a=leo&pede=pellentesque&posuere=ultrices&nonummy=mattis&integer=odio&non=donec&velit=vitae&donec=nisi&diam=nam'
+	},
+	{
+		title: 'ligula in',
+		artist: 'Emmet Farriar',
+		url: 'https://posterous.com/erat/curabitur/gravida/nisi/at.jsp?mus=dui&etiam=proin&vel=leo&augue=odio&vestibulum=porttitor&rutrum=id&rutrum=consequat&neque=in&aenean=consequat&auctor=ut&gravida=nulla&sem=sed&praesent=accumsan&id=felis&massa=ut&id=at&nisl=dolor&venenatis=quis&lacinia=odio&aenean=consequat&sit=varius&amet=integer&justo=ac&morbi=leo&ut=pellentesque&odio=ultrices&cras=mattis&mi=odio&pede=donec&malesuada=vitae&in=nisi&imperdiet=nam&et=ultrices&commodo=libero&vulputate=non&justo=mattis&in=pulvinar&blandit=nulla&ultrices=pede&enim=ullamcorper&lorem=augue&ipsum=a&dolor=suscipit&sit=nulla&amet=elit&consectetuer=ac&adipiscing=nulla&elit=sed'
+	}
+];
