34e2e2d5fa694dfec6e1d7bdc4344cf59547c2ff
binitasoni <binita.soni@somaiya.edu>
2024-02-24 14:07:41 +0000

    reformating folders

--- backend/Spotify API/search_songs.py
+++ backend/Spotify API/search_songs.py
@@ -5,7 +5,12 @@ track_id="https://open.spotify.com/track/2gkVEnpahpE3bQuvGuCpAV"
 track=auth_manager.sp.track(track_id, market=None)
 print(track)
 import json
-with open('data.json', 'w') as f:
+with open('song_data.json', 'w') as f:
     json.dump(track, f)
+# song = 
+# album/images[2]/url
+# artists[0]/name
+# external_urls/spotify
+# id
 # result['tracks']['items'][0]['artists']
 # print(result)56
