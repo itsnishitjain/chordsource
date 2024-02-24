5de0bc2ec7b6a0649fb7c9493bd6c14e4e68ad24
Hari <hprasad2709@gmail.com>
2024-02-24 15:46:11 +0000

    added thumbnail images

--- frontend/app/src/components/Song.svelte
+++ frontend/app/src/components/Song.svelte
@@ -6,11 +6,18 @@
 </script>
 
 <div class="flex p-2 justify-between border border-slate-700 bg-slate-800 rounded-md mt-2">
-	<div class="flex flex-col">
-		<span class="font-bold"
-			><a rel="noopener noreferrer" href={songData.url}>{songData.title}</a></span
-		>
-		<span class="italic">by {songData.artist}</span>
+	<div class="flex gap-4">
+		<img
+			src={songData}
+			alt="thumb nail"
+			class="w-16 aspect-square rounded-sm overflow-hidden bg-pink-300"
+		/>
+		<div class="flex flex-col">
+			<span class="font-bold"
+				><a rel="noopener noreferrer" href={songData.url}>{songData.title}</a></span
+			>
+			<span class="italic">by {songData.artist}</span>
+		</div>
 	</div>
 	{#if votable}
 		<div class="flex align-middle gap-4 pt-3">
