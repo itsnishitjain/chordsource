e0b3b438c90bed2614be9cd7a569301f60bf3360
nishitjainalt <nijain@tcd.ie>
2024-02-24 18:13:53 +0000

    pieChart

---
+++ frontend/app/src/components/History.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Song from './Song.svelte';
+
+	import dummyData from '$lib/dummyData';
+
+	let songData = dummyData.slice(4);
+</script>
+
+<div class="max-w-md ml-auto mr-auto">
+	<div class="mb-24">
+		{#each songData as song}
+			<Song songData={song} votable={false} />
+		{/each}
+	</div>
+</div>
---
+++ frontend/app/src/routes/[slug]/analytics/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+	import Form from '../../../components/Form.svelte';
+	import Music from '../../../components/icons/Music.svelte';
+	import Chart from '../../../components/icons/Chart.svelte';
+	import History from '../../../components/History.svelte';
+	import Pie from '../../../components/Pie.svelte';
+</script>
+
+<header>
+	<div class="navbar bg-base-300">
+		<div class="flex-1">
+			<a href="/" class="btn btn-ghost text-xl">hello</a>
+		</div>
+		<div class="flex-none">
+			<div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+				<div class="indicator">
+					<Chart />
+				</div>
+			</div>
+			<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+				<div>
+					<a href="/[slug]"><Music /></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+<body class="bg-gradient-to-b from-pink-300 to-cyan-500">
+	<div class="bg-gradient-to-t from-cyan-500 to-blue-500"></div>
+</body>
+
+<!-- now playing -->
+<section class="p-2 mb-4 pt-8">
+	<div class="max-w-md ml-auto mr-auto">
+		<h1 class="font-bold text-xl pb-6 text-base-300">Current Mood</h1>
+	</div>
+</section>
+
+<!-- up next -->
+<section class="p-2 mb-4">
+	<div class="max-w-md ml-auto mr-auto">
+		<h1 class="font-bold text-xl pb-6 text-base-300">Today's Vibe</h1>
+		<Pie />
+	</div>
+</section>
+
+<!-- leaderboard -->
+<section class="p-2 mb-4">
+	<div class="max-w-md ml-auto mr-auto">
+		<h1 class="font-bold text-xl pb-6 text-base-300">History</h1>
+		<History />
+	</div>
+</section>
