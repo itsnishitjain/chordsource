3ba9279a789a24300c8f07aab4d2dbcf51ffd833
hannah <luuh@tcd.ie>
2024-02-24 16:22:39 +0000

    fixed form

--- frontend/app/src/components/Form.svelte
+++ frontend/app/src/components/Form.svelte
@@ -6,21 +6,15 @@
 	};
 </script>
 
-<form
-	class="fixed bottom-0 left-0 right-0 p-4 max-h-24 flex justify-center align-middle bg-base-300"
-	on:submit={(e) => addSong(e)}
->
-	<div class="pr-16">
-		<label>
-			<input
-				bind:value={songUrl}
-				type="url"
-				name="spotify_song_url"
-				id="song_url"
-				placeholder="Enter a spotify link"
-				class="input w-svw max-w-xs rounded-md p-2"
-			/>
-		</label>
-	</div>
-	<button class="btn btn-outline" type="submit">Add</button>
+<form class="fixed bottom-0 left-0 right-0 p-4 gap-4 justify-center max-h-24 flex bg-base-300"
+	on:submit={(e) => addSong(e)}>
+	<input
+		bind:value={songUrl}
+		type="url"
+		name="spotify_song_url"
+		id="song_url"
+		placeholder="Enter a spotify link"
+		class="input w-svw max-w-xs shrink rounded-md p-2"
+	/>
+	<button class="btn btn-outline flex" type="submit">Add</button>
 </form>
